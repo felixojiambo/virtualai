204da4b2a43ecee6fb1bd387d601fd403027f094
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-02 08:09:49 -0700

    Implemented auth middleware with custom route configurations

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+    publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'],
+  ignoredRoutes: ['/chatbot'],
+})
+export const config = {
+    matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  }
