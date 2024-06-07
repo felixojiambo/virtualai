6f3b91a1d3222e06c2c7f1db0b34c2df6eadd2eb
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-07 04:42:05 -0700

    overview

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-    publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'],
+  publicRoutes: [
+    '/', 
+    '/auth(.*)', 
+    '/portal(.*)', 
+    '/images(.*)',
+    '/vercel.svg', 
+    '/next.svg', 
+    '/favicon.ico'
+  ],
   ignoredRoutes: ['/chatbot'],
-})
+});
+
 export const config = {
-    matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-  }
+  matcher: [
+    '/((?!.*\\.[\\w]+$|_next).*)', 
+    '/', 
+    '/(api|trpc)(.*)'
+  ],
+};
